3c022ff6b5dc284530c620bee3ac149ec47a956b
Brett Turing <bjsherman80@gmail.com>
2020-07-07 18:18:25 -0600

    Add methods to cell.rb

--- lib/cell.rb
+++ lib/cell.rb
@@ -1,11 +1,47 @@
-def Cell
 
-  attr_reader :location
-  def initialize(location)
-    @location = location
+
+class Cell
+
+  attr_reader :coordinate, :empty, :ship, :render
+  def initialize(coordinate)
+    @coordinate = coordinate.upcase
+    # @letter = coordinate[0].upcase
+    # @number = coordinate[1]
+    @render = "."
+    @ship = nil
   end
 
-  def 
+  def empty?
+    if ship == nil
+      true
+    else
+      false
+    end
+  end
+
+  def place_ship(ship_object)
+    @ship = ship_object
+  end
+
+  def fired_upon?
+    if render == "."
+      false
+    else
+      true
+    end
+  end
+
+  def fire_upon
+    if empty?
+      @render = "M"
+    elsif ship.health > 1
+      @render = "H"
+      ship.hit
+    else
+      @render = "X"
+      ship.hit
+    end
+  end
 
 
 
