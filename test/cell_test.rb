a9694c7b2fd5ec54df3285a0c781d3340f2de684
Hope Gochnour <hope.gochnour@gmail.com>
2020-07-07 21:42:57 -0600

    Update this branch with Main changes

--- test/cell_test.rb
+++ test/cell_test.rb
@@ -1,87 +1,68 @@
-require "./lib/ship"
+"./lib/ship"
 require "./lib/cell"
 require "minitest/autorun"
 require "minitest/pride"
 
 class CellTest < MiniTest::Test
 
-  def test_it_is_a_cell
-    cell = Cell.new("B4")
+ def test_it_is_a_cell
+   cell = Cell.new("B4")
 
-    assert_instance_of Cell, cell
-  end
+   assert_instance_of Cell, cell
+ end
 
-  def test_it_has_attributes
-    cell = Cell.new("B4")
+ def test_it_has_attributes
+   cell = Cell.new("B4")
 
-    assert_equal "B4", cell.coordinate
-    assert_nil(cell.ship)
-  end
+   assert_equal "B4", cell.coordinate
+   assert_nil(cell.ship)
+ end
 
-  def test_a_ship_can_be_added
-    cruiser = Ship.new("Cruiser", 3)
-    cell = Cell.new("B4")
+ def test_a_ship_can_be_added
+   cruiser = Ship.new("Cruiser", 3)
+   cell = Cell.new("B4")
 
-    assert_equal true, cell.empty?
+   assert_equal true, cell.empty?
 
-    cell.place_ship(cruiser)
+   cell.place_ship(cruiser)
 
-    assert_equal false, cell.empty?
-  end
+   assert_equal false, cell.empty?
+ end
 
-  def test_has_been_fired_upon
-    cruiser = Ship.new("Cruiser", 3)
-    cell = Cell.new("B4")
+ def test_has_been_fired_upon
+   cruiser = Ship.new("Cruiser", 3)
+   cell = Cell.new("B4")
 
-    cell.place_ship(cruiser)
+   cell.place_ship(cruiser)
 
-    assert_equal false, cell.fired_upon?
-  end
+   assert_equal false, cell.fired_upon?
+ end
 
-  def test_can_be_fired_upon
-    cruiser = Ship.new("Cruiser", 3)
-    cell = Cell.new("B4")
+ def test_can_be_fired_upon
+   cruiser = Ship.new("Cruiser", 3)
+   cell = Cell.new("B4")
 
-    cell.place_ship(cruiser)
-    cell.fire_upon
-    assert_equal true, cell.fired_upon?
-    assert_equal 2, cell.ship.health
-  end
+   cell.place_ship(cruiser)
+   cell.fire_upon
+   assert_equal true, cell.fired_upon?
+   assert_equal 2, cell.ship.health
+ end
 
-  def test_cell_render
-    cruiser = Ship.new("Cruiser", 3)
-    cell = Cell.new("B4")
+ def test_cell_render
+   cruiser = Ship.new("Cruiser", 3)
+   cell = Cell.new("B4")
 
-    cell.place_ship(cruiser)
-    cell.fire_upon
-    assert_equal "H", cell.render
-  end
+   cell.place_ship(cruiser)
+   cell.fire_upon
+   assert_equal "H", cell.render
+ end
 
-  def test_revealing_ships
-    cruiser = Ship.new("Cruiser", 3)
-    cell = Cell.new("B4")
+ def test_revealing_ships
+   cruiser = Ship.new("Cruiser", 3)
+   cell = Cell.new("B4")
 
-    cell.place_ship(cruiser)
-    cell.render(true)
-    asssert_equal "S", cell.render
-  end
-
-  def test_fire_upon
-    cell = Cell.new("B4")
-    cruiser = Ship.new("Cruiser", 3)
-
-
-  def test_it_can_render
-    skip
-    cell = Cell.new("B4")
-    cruiser = Ship.new("Cruiser", 3)
-
-    # render can check if cell is empty
-    # if false, run ship.hit and return "H"
-    # if true, returns "M"
-    # render's RVs: H (hit), M (miss), . (fired upon), X (sunk_)
-    # render will also replace H's
-
-
-  end
+   cell.place_ship(cruiser)
+   cell.render(true)
+   asssert_equal "S", cell.render
+ end
 end
