6406c288cde1df1971f20ffe8ba27e1ac5350cc0
Hope Gochnour <hope.gochnour@gmail.com>
2020-07-09 22:04:10 -0600

    Add placement methods to board.rb

--- lib/board.rb
+++ lib/board.rb
@@ -20,6 +20,25 @@ class Board
     end
   end
 
+  def orientation_check(placement)
+    place_letters = []
+    place_numbers = []
+    placement.each do |place|
+      place_letters << place[0]
+      place_numbers << place[1].to_i
+    end
+    comparing_letters = place_letters & ("A".."D").to_a
+    comparing_numbers = place_numbers & (1..4).to_a
+    if (comparing_letters.count >= 2) && (comparing_numbers.count >= 2)
+      false
+    elsif (comparing_letters.count >= 2) && (comparing_numbers.count == 1)
+      (comparing_letters.first..comparing_letters.last).to_a == comparing_letters
+    elsif (comparing_numbers.count >= 2) && (comparing_letters.count == 1)
+      (comparing_numbers.first..comparing_numbers.last).to_a == comparing_numbers
+    else
+    end
+  end
+
   def valid_coordinate?(coord)
     cells.one? do |coordinate, cell|
       cell.coordinate == coord
@@ -27,15 +46,28 @@ class Board
   end
 
   def valid_placement?(ship, placement)
-    num = ship.length
     spot_check = placement.all? do |spot|
       valid_coordinate?(spot)
     end
-    if (num == placement.length) && (spot_check == true)
-      placement.each_cons(num) { |place| (place.ord == (65) && (place.to_i == [1..4])) || (place.ord[65..68])} #49-52
+    if (ship.length == placement.length) && (spot_check == true)
+      orientation_check(placement)
     else
       false
     end
   end
 
+  def place(ship, placement)
+    if valid_placement?(ship, placement)
+      placement.each do |shipping|
+        @cells[shipping].place_ship(ship)
+      end
+    end
+  end
+
+  def render(arg = false)
+    if arg = false
+      @cells.keys.each
+    elsif arg = true
+    end
+  end
 end
