c3177752bd2d7433ac71ef957e9ebb441c7bbc45
Hope Gochnour <hope.gochnour@gmail.com>
2020-07-15 14:09:00 -0600

    Rename method add to Player, test add to player_test for it, game additions

--- lib/player.rb
+++ lib/player.rb
@@ -1,8 +1,16 @@
+require "./lib/ship"
+require "./lib/cell"
+require "./lib/board"
+
 class Player
 
   attr_reader :name, :board
   def initialize(name, board)
     @name = name
     @board = board
+  end
+
+  def rename(new_name)
+    @name = new_name
   end
 end
